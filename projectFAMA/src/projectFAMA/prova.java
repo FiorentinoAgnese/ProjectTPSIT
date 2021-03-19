847e5e34dc3510cd783855b444d23dc6a4f93943
corsa <corsa@LAPTOP-1QBGBL4D.station>
2021-03-19 12:45:02 +0100

    Prova 2.0

--- projectFAMA/src/projectFAMA/prova.java
+++ projectFAMA/src/projectFAMA/prova.java
@@ -1,10 +1,14 @@
 package projectFAMA;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+
 public class prova {
 
 	public static void main(String[] args) {
 		System.out.println("Matteo");
 		System.out.println("Matteo2");
+		InternetAddress it;
 	}
 
 }
